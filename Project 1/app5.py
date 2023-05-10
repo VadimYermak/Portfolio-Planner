533a8caf2df7056c86403536f06ea712586f0cc6
Philip Timothe <49532749+PhilipTimothe@users.noreply.github.com>
2023-05-10 18:45:58 -0400

    Update app5.py

--- Project 1/app5.py
+++ Project 1/app5.py
@@ -74,15 +74,13 @@ def get_sharpe_ratio(ticker):
     annual_standard_deviation = daily_returns.std() * np.sqrt(1260)
     sharpe_ratio = annual_average_return / annual_standard_deviation
     
-    print(f"Sharpe ratio for {ticker} is {sharpe_ratio : .02f}")
+    print(f"Sharpe ratio for {ticker} is {sharpe_ratio : .02f}.")
     print(f"Annualized Average Return is {annual_average_return : .02f}")
     print(f"Annualized Standard Deviation is {annual_standard_deviation : .02f}.")
     
+
 # Create a dataframe from two stock inputs:
 def df_portfolio(stock1, stock2):
-    alpaca_api_key = os.getenv("key")
-    alpaca_secret_key = os.getenv("secret")
-
     alpaca = tradeapi.REST(alpaca_api_key, alpaca_secret_key, api_version = "v2")
 
     tickers = [stock1, stock2]
@@ -98,6 +96,7 @@ def df_portfolio(stock1, stock2):
     df_portfolio = pd.concat([stock1_df, stock2_df], axis=1, keys=[stock1, stock2])
     
     return df_portfolio
+
 
 # Monte Carlo simulation with df_portfolio defined as a parameter
 def mc_sim(df_portfolio, weight1, weight2, investment_years, initial_investment):
@@ -118,21 +117,22 @@ def mc_sim(df_portfolio, weight1, weight2, investment_years, initial_investment)
                  f"The lowest value of your portfolio is {ci_95_lower_cumulative_return: .02f}.", 
                  f"The highest value of your portfolio is {ci_95_upper_cumulative_return: .02f}.", 
                  )
-    
-    
+
+
 # Application run functionality    
 def run():
     if user_name == "":
         user_intro()
     
     cruddy_cli_running = True
-    
+
     while cruddy_cli_running:
         choice = questionary.select(
             "Where would you like to continue?",
             choices=["Research Stocks", "Run Monte Carlo Simulation", "Favorites List", "Quit"]
         ).ask()
         
+        # Research Stock functionality
         if choice == "Research Stocks":
             print("\nHere you can choose a stock to research\n")
             print("If you are satisfied with the data you can add it to your favorites or move on to the next module\n")
@@ -161,7 +161,7 @@ def run():
                     "Exxon Mobil Corp",
                     "QUIT - not stock :("
                 ]).ask()
-        
+            
             if choice != "QUIT - not stock :(":
                 get_sharpe_ratio(stocks[choice])
                 favorite_stock = questionary.select("\nWould you like to add to favorites?\n", choices = ["Yes", "No"]).ask()
@@ -176,28 +176,38 @@ def run():
             else:
                 cruddy_cli_running = False
                 print("\nThank you for using the CRUDdy CLI! Goodbye!")
-                
-        elif choice == "Run Monte Carlo Simulation":
-            print("This module will perform a simulation of your portfolio performance. Please answer the following questions.")
-    
         
+        
+        # Run Monte Carlo Simulation
+        elif choice == "Run Monte Carlo Simulation":
+            print("This module will perform a simulation of your portfolio performance. Please answer the following questions.\n")
+            
+            
+    
+            # Gather user information for simulation
             initial_investment = questionary.text("How much money (USD) are you investing in this portfolio?").ask()
             investment_years = questionary.text("How many years will you be investing?").ask()
             stock1 = stocks[questionary.select("Select a stock for your portfolio", choices = stocks).ask()]
             stock2 = stocks[questionary.select("Select a second stock for your portfolio", choices = stocks).ask()]
             
+            # Stock Choice Validation
+            while stock1 == stock2:
+                print("Sorry please choose another stock, you chose this one already\n")
+                stock2 = stocks[questionary.select("Select a second stock for your portfolio", choices = stocks).ask()]
+            
             portfolio_weights = questionary.select(
                 f"Would you like to this to be an equally-weighted portfolio? (ie: .50 {stock1} and .50 {stock2})?",
                 choices=["Yes", "No",]).ask()
 
+            # Portfolio weights check
             if portfolio_weights == "Yes":
                 weight1 = .50
                 weight2 = .50
-            
             else:
                 weight1 = float(questionary.text(
                     f"What percentage of the portfolio will be made up of {stock1}? Please enter the percentage in decimal format.").ask())
-                confirm = questionary.select(f"The remainder of the portfolio will comprise of {1.0 - weight1 : .02}. Is this correct?", choices = ["Yes", "No"]).ask()
+                confirm = questionary.select(
+                    f"The remainder of the portfolio will comprise of {1.0 - weight1 : .02}. Is this correct?", choices = ["Yes", "No"]).ask()
         
                 if confirm == "Yes":
                     weight2 = 1.0 - weight1
@@ -223,7 +233,9 @@ def run():
                 else:
                     cruddy_cli_running = False
                     print("\nThank you for using the CRUDdy CLI! Goodbye!")
+
                     
+        # Add to favorite list
         elif choice == "Favorites List":
             if favorited_stocks == []:
                 print("Favorites list is empty\n")
